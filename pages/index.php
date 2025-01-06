30c9e300e81b947679d9ce9a0031cd0e6bb3c432
gaksss <clement.carlier2@gmail.com>
2025-01-06 10:57:34 +0100

    DONE: setup tailwind

--- pages/index.php
+++ pages/index.php
@@ -4,8 +4,10 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>BookMarket</title>
+    <link rel="stylesheet" href="../assets/styles/output.css">
+
 </head>
 <body>
-    
+    <h1 class=" bg-red-600 h-20 font-extrabold">Test</h1>
 </body>
 </html>
