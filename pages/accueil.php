b554ef7f2f6c85a4a4d5a1805f26eed6f9913419
gaksss <clement.carlier2@gmail.com>
2025-01-06 11:39:04 +0100

    DONE : navbar

---
+++ pages/accueil.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>BookMarket</title>
+    <link rel="stylesheet" href="../assets/styles/output.css">
+    <link href='https://unpkg.com/boxicons@2.1.4/css/boxicons.min.css' rel='stylesheet'>
+
+</head>
+
+<body>
+    <header class=" bg-primary-dark h-16 font-extrabold flex items-center px-6 justify-between w-full md:px-12 md:h-24">
+
+        <a href="#" class="flex items-center"><img src="../assets/img/logo.png" alt="Logo de la marque représentant la Terre sur un livre" class="h-12 w-12 md:h-20 md:w-20 filter brightness-200 ">
+            <h1 class="text-4xl text-primary-white hidden md:flex">BookMarket</h1>
+        </a>
+
+        <nav class="flex justify-evenly w-[150px] md:w-[250px] lg:w-[300px]">
+
+            <a href="#"><i class='bx bx-cart text-4xl text-primary-white md:text-6xl'></i></a>
+            <a href="#"><i class='bx bx-search-alt-2 text-4xl text-primary-white md:text-6xl'></i></a>
+            <a href="#"><i class='bx bx-user text-4xl text-primary-white md:text-6xl'></i></a>
+        </nav>
+    </header>
+</body>
+
+</html>
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,12 @@
 module.exports = {
   content: ["./pages/*"],
   theme: {
+    colors: {
+      "primary-dark": "#262626",
+      "primary-white": "#F0EDEE",
+      "green": "#538083",
+      "red": "#B75D69"
+    },
     extend: {},
   },
   plugins: [],
